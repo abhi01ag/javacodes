b63c01c49256e17034129364bb085c5a3aaa79d6
abhi01ag <abhishekk.ec.21@nitj.ac.in>
2023-06-11 16:21:50 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("this is the program  to calculate the percentage");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the total marks");
+        float total = sc.nextInt();
+        System.out.println("enter the marks in english");
+        int english = sc.nextInt();
+        System.out.println("enter the marks in maths");
+        int maths = sc.nextInt();
+        System.out.println("enter the marks in sst");
+        int sst = sc.nextInt();
+        System.out.println("enter the marks in science");
+        int science = sc.nextInt();
+        System.out.println("enter the marks in hindi");
+        int hindi = sc.nextInt();
+        System.out.println("total marks obtained is");
+        float totalobtained = english+maths+science+sst+hindi;
+        System.out.println(totalobtained);
+        float percentage = (totalobtained/total)*100;
+        System.out.print("your percentage is ");
+        System.out.println(percentage);
+
+
+
+    }
+}
---
+++ yes2nd.java
@@ -0,0 +1,26 @@
+package com.company;
+import java.util.Scanner;
+
+public class yes2nd {
+    public static void main(String[] args) {
+        //System.out.println("my name");
+        Scanner abhi = new Scanner(System.in);
+        //int i = abhi.nextInt();
+        //System.out.println(i++);
+        //System.out.println(++i);
+        //System.out.println(i++ + ++i);
+        //String name = "abhishek";
+        //System.out.println(name);
+        String name = abhi.nextLine();
+        String lwer = name.toLowerCase();
+        String lwe1 = name.replace(' ','_');
+        String rpl1 = name.replace("<|name|>","Abhsihek");
+        System.out.println(lwer);
+        System.out.println(lwe1);
+        System.out.println(rpl1);
+        System.out.println(name.indexOf("  "));
+        System.out.println(name.indexOf("   "));
+
+
+    }
+}
